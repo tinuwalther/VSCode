adb24a63e05e49c5945514984bb910ba2d6c90d0
Martin Walther <it@martin-walther.ch>
2023-07-12 15:06:13 +0200

    Update profile

---
+++ Data/profile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,27 @@
+function Test-IsAdministrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function prompt
+{
+
+    if (Test-IsAdministrator) {
+        $color = 'Red'
+    }
+    else{
+        $color = 'Green'
+    }
+    Set-Location ~
+    $history = Get-History -ErrorAction Ignore
+    $Version = "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)"
+    Write-Host "[$($history.count[-1])] " -NoNewline
+    Write-Host "[$(($env:UserName).ToUpper())@$($env:COMPUTERNAME)] " -ForegroundColor $color -NoNewline
+    Write-Host ("I ") -nonewline
+    Write-Host (([char]9829) ) -ForegroundColor $color -nonewline
+    Write-Host (" PS $Version ") -nonewline
+    Write-Host ("$(get-location) ") -foregroundcolor $color -nonewline
+    Write-Host (">") -nonewline
+    return " "
+
+}
---
+++ Data/profile/Microsoft.VSCode_profile.ps1
@@ -0,0 +1,27 @@
+function Test-IsAdministrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function prompt
+{
+
+    if (Test-IsAdministrator) {
+        $color = 'Red'
+    }
+    else{
+        $color = 'Green'
+    }
+    $vscode = (code --version)[0]
+    $history = Get-History -ErrorAction Ignore
+    $Version = "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)"
+    Write-Host "[$($history.count[-1])] " -NoNewline
+    Write-Host "[$(($env:UserName).ToUpper())@$($env:COMPUTERNAME)] [VSCode $($vscode)] " -ForegroundColor $color -NoNewline
+    Write-Host ("I ") -nonewline
+    Write-Host (([char]9829) ) -ForegroundColor $color -nonewline
+    Write-Host (" PS $Version ") -nonewline
+    Write-Host ("$(get-location) ") -foregroundcolor $color -nonewline
+    Write-Host (">") -nonewline
+    return " "
+
+}
